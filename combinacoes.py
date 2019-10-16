494017a3fe1a9ef18a6cadcf36c5d58e6cf4f8c3
fakebmarchete <afdlf2@gmail.com>
2019-10-16 10:51:03 -0300

    Adicionado loop para fornecimento optativo de frases

--- combinacoes.py
+++ combinacoes.py
@@ -1,34 +1,46 @@
+'''
+requisitos importar 'itertools'
+'''
+
 import itertools
 
 mapa = { 0:" ", 1:", "}
-combinacoes = []
 
-frase = input("Forneça uma frase: ")
-listaPalavras = frase.split()
-# print("listaPalavras=",listaPalavras)
-# print("len(listaPalavras)=",len(listaPalavras))
+entrada = 'inicio'
 
-# gerando o arranjo de possiblidades (com repetição) 
-possibilidades = list(itertools.product([0,1], repeat=len(listaPalavras)-1))
-# print("possibilidades=",possibilidades)
-# print("len(possibilidades)=",len(possibilidades))
+while entrada != '0':
+    entrada = input("Forneça uma frase (Digite '0' para encerrar): ")
+    if entrada != '0':
+        listaPalavras = entrada.split()
+        # print("listaPalavras=",listaPalavras)
+        # print("len(listaPalavras)=",len(listaPalavras))
 
-for arranjo in possibilidades:
-    # print("arranjo=",arranjo)
-    frases = []
-    i=0
-    frase=""
-    for palavra in listaPalavras:
-        # print("\tpalavra=",palavra)
-        if i < len(listaPalavras)-1:
-            frase += palavra + mapa[arranjo[i]]    
-        else:
-            frase += palavra
-        i=i+1
-    # print("\tfrase= "+frase)
-    frases.append(frase)
-    combinacoes.append(frases)
+        # gerando o arranjo de possiblidades (com repetição) 
+        possibilidades = list(itertools.product([0,1], repeat=len(listaPalavras)-1))
+        # print("possibilidades=",possibilidades)
+        # print("len(possibilidades)=",len(possibilidades))
 
-print("combinacoes=",combinacoes)
+        # outra abordagem
+        # bin = [0,1]
+        # possibilidades = [ (x,y,z) for x in bin for y in bin for z in bin ]
+
+        combinacoes = []
+        for arranjo in possibilidades:
+            # print("arranjo=",arranjo)
+            frases = []
+            i=0
+            frase=""
+            for palavra in listaPalavras:
+                # print("\tpalavra=",palavra)
+                if i < len(listaPalavras)-1:
+                    frase += palavra + mapa[arranjo[i]]    
+                else:
+                    frase += palavra
+                i=i+1
+            # print("\tfrase= "+frase)
+            frases.append(frase)
+            combinacoes.append(frases)
+
+        print("combinacoes =",combinacoes)
 
 
